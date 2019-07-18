8da0066c3dabc3f6948001d50d50453d4d8b357b
slegrand <ut5svsxgc@relay.firefox.com>
2019-07-18 15:52:14 -0400

    Initial Commit

---
+++ src/pyfsm/__about__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the Apache License 2.0
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/licenses/Apache-2.0
+#
+# @author	Sebastien LEGRAND
+# @license	Apache License 2.0
+#
+# @brief	Version file
+
+# ----- globals
+__version__ = "1.0.0"
---
+++ src/pyfsm/fsm.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the Apache License 2.0
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/licenses/Apache-2.0
+#
+# @author	Sebastien LEGRAND
+# @license	Apache License 2.0
+#
+# @brief	Main file
+
+# ----- imports
+from __future__ import annotations
+from typing import Any, Dict, List
+
+# ----- globals
+# ----- functions
+# ----- classes
+# ----- begin
